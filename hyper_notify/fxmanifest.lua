c8fd89f834f383e33de2352e43432a42e14582f5
Hyper <144830674+hyper0939@users.noreply.github.com>
2026-06-28 19:20:41 +0200

    Changes
    Added some files +/-

---
+++ hyper_notify/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Hyper"
+version "0.0.1"
+
+client_scripts {
+    "Code/client-side.lua",
+}
+
+server_scripts {
+    "Code/server-side.lua",
+}
+
+shared_scripts {
+    "config.lua"
+}
+
+ui_page "UI/index.html"
+
+files {
+    "UI/*.html",
+    "UI/*.css",
+    "UI/fonts/*.otf",
+    "UI/fonts/*.woff"
+}
